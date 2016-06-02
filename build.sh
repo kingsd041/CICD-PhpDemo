6cd4ec38f55adbeb73f134ec1f0a8e218564665a
海龙 王 <wanghl@neunn.com>
2016-06-02 02:31:16 +0000

    1

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo \$BUILD_NUMBER1=$BUILD_NUMBER
+echo \$WORKSPACE1=$WORKSPACE
+sh $WORKSPACE/test-build/test-build.sh 
---
+++ test-build/test-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ${WORKSPACE}/src
+docker build -t registry.rancher.com/python-redis-demo:v${BUILD_NUMBER} .
+docker push registry.rancher.com/python-redis-demo:v${BUILD_NUMBER}
+
+cd ${WORKSPACE}/test-build
+sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
+sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
+rancher-compose --url http://103.39.107.62:8080 --access-key BCCA0297BADDA26DFC2E --secret-key H2P5p1cdtpNU1Ntj5p6WEYQvbW2febgMv9rQKSp1 -p python-redis-demo-build${BUILD_NUMBER} up -d
